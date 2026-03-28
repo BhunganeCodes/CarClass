dd2a81064420b9f18e03e95f50cb9501abaaddef
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-03-28 20:48:16 +0200

    Refactored Main class to account for changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
+        Car car = new Car("Toyota", 2026, 32000.99);
 
         System.out.println(car.isRunning);
         car.start();
