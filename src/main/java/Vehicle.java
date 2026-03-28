72bb867432a511534b546976e2b1ee77b7b7deef
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-03-28 20:40:08 +0200

    Added more attributes to the Vehicle class

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,6 +1,9 @@
 public abstract class Vehicle {
     private String name;
     private int topSpeed;
+    private int year;
+    private double price;
+    private boolean isRunning = false;
 
     public abstract void start();
     public abstract void stop();
